c3a11941cfe5cf2a4dd7b30d727feecf5ea2ff63
v.attanasio <v.attanasio@almaviva.it>
2024-12-12 16:04:27 +0100

    capacitor-logo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,19 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Bricoscan',
-  webDir: 'www',
+  appId: "io.ionic.starter",
+  appName: "Bricoscan",
+  webDir: "www",
   bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff", // Bianco
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
