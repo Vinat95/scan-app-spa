dd321ba5db77aed59e5bdeb79cf4981b28592eb9
Vincenzo Attanasio <vinat2000@gmail.com>
2025-01-27 16:43:22 +0100

    .

--- src/app/product-table/product-table.page.ts
+++ src/app/product-table/product-table.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Product, Products } from "types/product";
 import { Prodotti } from "src/data/source";
 import { AlertController, NavController } from "@ionic/angular";
